7ad1cd5e174880955e376c3ad6ccaa7d03b036db
HAI <hai@M164-NKF-Laptop.local>
2015-12-16 15:40:09 -0500

    Initial Commit

---
+++ Picture Dot Puzzle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Picture Dot Puzzle
+//
+//  Created by HAI on 12/16/15.
+//  Copyright (c) 2015 Nathan Fennel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Picture Dot PuzzleTests/Picture_Dot_PuzzleTests.m
@@ -0,0 +1,40 @@
+//
+//  Picture_Dot_PuzzleTests.m
+//  Picture Dot PuzzleTests
+//
+//  Created by HAI on 12/16/15.
+//  Copyright (c) 2015 Nathan Fennel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Picture_Dot_PuzzleTests : XCTestCase
+
+@end
+
+@implementation Picture_Dot_PuzzleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
