0f883cb324980b7327bd5e7678c256926d838016
Nathan Fennel <nathanfennel@gmail.com>
2015-12-16 18:28:09 -0500

    Added Dot View The dot view contains all of the logic behind touching and dividing

---
+++ Picture Dot Puzzle/PDPDotView.h
@@ -0,0 +1,13 @@
+//
+//  PDPDotView.h
+//  
+//
+//  Created by HAI on 12/16/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PDPDotView : UIView
+
+@end
---
+++ Picture Dot Puzzle/PDPDotView.m
@@ -0,0 +1,21 @@
+//
+//  PDPDotView.m
+//  
+//
+//  Created by HAI on 12/16/15.
+//
+//
+
+#import "PDPDotView.h"
+
+@implementation PDPDotView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
