90a6e63003179d8d49089dfc46bc028a4048fc59
Nathan Fennel <nathanfennel@gmail.com>
2015-12-16 20:52:55 -0500

    Added Data Manager Singleton

---
+++ PDPDataManager.h
@@ -0,0 +1,13 @@
+//
+//  PDPDataManager.h
+//  
+//
+//  Created by HAI on 12/16/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PDPDataManager : NSObject
+
+@end
---
+++ PDPDataManager.m
@@ -0,0 +1,13 @@
+//
+//  PDPDataManager.m
+//  
+//
+//  Created by HAI on 12/16/15.
+//
+//
+
+#import "PDPDataManager.h"
+
+@implementation PDPDataManager
+
+@end
